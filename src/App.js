8b65e71cd49a43ca2dfe5156f25eddd178168630
pratikrokde <rokdepratik3@gmail.com>
2021-03-07 23:23:24 +0530

    search movie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchData, sortByName, sortByRating } from "./actions";
+import {
+  fetchData,
+  fetchDataForSearch,
+  sortByName,
+  sortByRating,
+} from "./actions";
 import "./App.css";
 import Movies from "./Movies";
 import SearchInput from "./SearchInput";
@@ -10,6 +15,7 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 // const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const App = () => {
+  const [inputMovie, setInputMovie] = useState();
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
@@ -24,7 +30,16 @@ const App = () => {
   const sortByNameHandler = () => {
     dispatch(sortByName());
   };
-
+  //movie by name -> search
+  const getInputValue = (e) => {
+    setInputMovie(e.target.value);
+  };
+  // seach movie -> submit btn
+  const submitBtnHandler = (e) => {
+    e.preventDefault();
+    dispatch(fetchDataForSearch(inputMovie));
+  };
+  // show movies on page render
   const mapMoviesHandler = () => {
     if (state.loading === true) {
       return <h4>Loading</h4>;
@@ -45,9 +60,7 @@ const App = () => {
           sortName={sortByNameHandler}
           sortRating={sortByRatingHandler}
         />
-        <SearchInput
-        //  change={getInputValue} click={submitBtnHandler}
-        />
+        <SearchInput change={getInputValue} click={submitBtnHandler} />
       </div>
       <div className="App">{mapMoviesHandler()}</div>
     </div>
--- src/actions.js
+++ src/actions.js
@@ -32,3 +32,29 @@ export const sortByName = () => {
     type: "SORT_BY_NAME",
   };
 };
+
+export const fetchDataForSearch = (inputMovie) => async (
+  dispatch,
+  getState
+) => {
+  dispatch({
+    type: "FETCH_DATA_REQUEST",
+  });
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${inputMovie}`
+    );
+    const data = await response.json();
+
+    dispatch({
+      type: "FETCH_DATA_SUCCESS",
+      payload: data.results,
+    });
+  } catch (error) {
+    dispatch({
+      type: "FETCH_DATA_FAILURE",
+      payload: error,
+    });
+  }
+};
